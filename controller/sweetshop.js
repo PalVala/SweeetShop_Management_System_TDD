c81d1cff0f14c28641ea4647506e33f3b498ff72
PalVala <valapal2005@gamil.com>
2025-07-17 04:19:52 +0530

    Function : add sweet using sweet class

--- controller/sweetshop.js
+++ controller/sweetshop.js
@@ -10,3 +10,21 @@ function addSweet(sweetData) {
   );
   sweets.push(sweet);
 }
+
+const { Sweet, sweets } = require('../model/sweet');
+
+function addSweet(sweetData) {
+  const { id, name, category, price, quantity } = sweetData;
+
+  
+  const exists = sweets.find(s => s.id === id);
+  if (exists) throw new Error('Sweet with this ID already exists');
+
+  const sweet = new Sweet(id, name, category, price, quantity);
+  sweets.push(sweet);
+}
+
+module.exports = {
+  addSweet,
+  
+};
