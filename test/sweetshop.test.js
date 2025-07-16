a00bc39d669756894a401d6ee27c0333748c873f
PalVala <valapal2005@gamil.com>
2025-07-17 04:15:40 +0530

    Test: add sweet

--- test/sweetshop.test.js
+++ test/sweetshop.test.js
@@ -1,7 +1,7 @@
 const { Sweet } = require('../model/sweet');
 
 describe('Sweet class', () => {
-  test('should create a Sweet object with correct properties', () => {
+  test('sweet object creation test', () => {
     const sweet = new Sweet(1, 'Kaju Katli', 'Nut-Based', 50, 20);
 
     expect(sweet.id).toBe(1);
@@ -11,3 +11,25 @@ describe('Sweet class', () => {
     expect(sweet.quantity).toBe(20);
   });
 });
+
+const { addSweet } = require('../controller/sweetshop');
+const sweets = require('../model/sweet');
+
+beforeEach(() => sweets.length = 0); // clear sweets before each test
+
+test('addSweet() should add a new sweet using Sweet class', () => {
+  const sweetData = {
+    id: 1,
+    name: 'Kaju Katli',
+    category: 'Nut-Based',
+    price: 50,
+    quantity: 20
+  };
+
+  addSweet(sweetData);
+
+  expect(sweets.length).toBe(1);
+  expect(sweets[0]).toMatchObject(sweetData);
+  expect(sweets[0]).toBeInstanceOf(Object);
+});
+
